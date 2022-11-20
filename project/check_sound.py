455c37cc11a77d9806a999e2babf6e8537533df9
Sason00 <72099659+Sason00@users.noreply.github.com>
2022-11-20 19:38:32 +0200

    Add files via upload

--- project/check_sound.py
+++ project/check_sound.py
@@ -1,28 +1,55 @@
-import wave
 import pyaudio
-
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100
-CHUNK = 4096
-
-audio = pyaudio.PyAudio()
-stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=CHUNK)
-
-frames = []
+import wave
  
-for i in range(0, 10000):
-    data = stream.read(CHUNK)
+chunk = 1024      # Each chunk will consist of 1024 samples
+sample_format = pyaudio.paInt16      # 16 bits per sample
+channels = 1      # Number of audio channels
+fs = 44100        # Record at 44100 samples per second
+time_in_seconds = 1
+filename = "soundsample.wav"
+ 
+p = pyaudio.PyAudio()  # Create an interface to PortAudio
+ 
+print('-----Now Recording-----')
+ 
+#Open a Stream with the values we just defined
+stream = p.open(format=sample_format,
+                channels = channels,
+                rate = fs,
+                frames_per_buffer = chunk,
+                input = True,
+                output=True)
+ 
+frames = []  # Initialize array to store frames
+
+print(f"{fs = }, {chunk = }, {time_in_seconds = }")
+print(int(fs / chunk * time_in_seconds))
+
+# Store data in chunks for 3 seconds
+for i in range(0, int(fs / chunk * time_in_seconds)):
+    data = stream.read(chunk)
     frames.append(data)
-    
-    
+ 
+# Stop and close the Stream and PyAudio
 stream.stop_stream()
 stream.close()
-audio.terminate()
+p.terminate()
  
-waveFile = wave.open("output.wav", 'wb')
-waveFile.setnchannels(CHANNELS)
-waveFile.setsampwidth(audio.get_sample_size(FORMAT))
-waveFile.setframerate(RATE)
-waveFile.writeframes(b''.join(frames))
-waveFile.close()
+print('-----Finished Recording-----')
+
+p = pyaudio.PyAudio()  # Create an interface to PortAudio
+stream = p.open(channels = 2,
+                format=sample_format,
+                rate = fs // 2, # somehow when it's normal it 2 times faster
+                frames_per_buffer = chunk,
+                output=True,
+                input=False,
+                output_device_index=4)
+
+for i in frames:
+    stream.write(i)
+
+stream.stop_stream()
+stream.close()
+p.terminate()
+
